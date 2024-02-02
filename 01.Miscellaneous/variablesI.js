5c8efeea099e8c86cf63ffebaa2e33c97340361b
math92n4 <math92n4@stud.kea.dk>
2024-02-02 10:03:56 +0100

    created introduction folder

--- 01.Miscellaneous/variablesI.js
+++ 01.Miscellaneous/variablesI.js
@@ -8,3 +8,18 @@ const hobbies = ["coding","eating"];
 hobbies.push("sleeping");
 me.hobbies = hobbies;
 console.log(me);
+
+// Types in JS: number, string, boolean, object, null, undefined, symbol, bigint
+console.log(typeof 22);
+console.log(typeof me.name);
+console.log(typeof true);
+console.log(typeof me)
+
+// Object: object, arrays, date
+console.log(typeof me.hobbies) // logs object
+
+// type coercin
+// we ALWAYS use strict equality checks:
+// === and !==
+console.log(2 == "2");
+console.log(2 === "2");
