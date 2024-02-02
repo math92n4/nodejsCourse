848d80615d1e58a220be6c8c58cd67eab22273f6
math92n4 <math92n4@stud.kea.dk>
2024-02-02 09:38:40 +0100

    created varibalesI file

---
+++ 01.Miscellaneous/variablesI.js
@@ -0,0 +1,10 @@
+const myName = "Mathias";
+const me = {};
+me.name = myName;
+// const is constant NOT in the value but in the assignment
+// meaning that it acnnot be reassigned and this also HAS to be assigned
+
+const hobbies = ["coding","eating"];
+hobbies.push("sleeping");
+me.hobbies = hobbies;
+console.log(me);
